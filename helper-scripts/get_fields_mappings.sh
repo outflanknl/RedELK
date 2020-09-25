ea707245db67d261adf0e688b83a223bd8c31058
Marc Smeets <marc@outflank.nl>
2020-09-25 19:13:44 +0200

    Updated width of headers and fields printing

--- helper-scripts/get_fields_mappings.sh
+++ helper-scripts/get_fields_mappings.sh
@@ -6,49 +6,49 @@
 # index redirtraffic
 type=redirtraffic
 outfile=redelk_fieldnames_${type}.md
-echo "|   Fieldname  |  ES field type   | Kibana field type    |  Comment  |" > $outfile
-echo "| --- | --- | --- | --- |" >> $outfile
-curl -s -X GET "localhost:5601/api/saved_objects/index-pattern/${type}" -H 'kbn-xsrf: true' | jq -r '(.attributes.fields | fromjson)[] | select(.name | startswith("_")|not)| select(.name | startswith("@")|not)|"|   " + .name + "   |   " + (.esTypes|tostring) + "   |   " + .type + "   |        |"'| tr -d '["]' >> $outfile
+echo "|   Fieldname                   |   ES type    |   Kibana type  |   Comment                                     | " > $outfile
+echo "| ----------------------------- | ------------ | -------------- | --------------------------------------------- | " >> $outfile
+curl -s -X GET "localhost:5601/api/saved_objects/index-pattern/${type}" -H 'kbn-xsrf: true' | jq -r '(.attributes.fields | fromjson)[] | select(.name | startswith("_")|not)| select(.name | startswith("@")|not)|"| " + .name + "   | " + (.esTypes|tostring) + "   | " + .type + "   |        |"'| tr -d '["]' >> $outfile
 
 
 # index rtops
 type=rtops
 outfile=redelk_fieldnames_${type}.md
-echo "|   Fieldname  |  ES field type   | Kibana field type    |  Comment  |" > $outfile
-echo "| --- | --- | --- | --- |" >> $outfile
-curl -s -X GET "localhost:5601/api/saved_objects/index-pattern/${type}" -H 'kbn-xsrf: true' | jq -r '(.attributes.fields | fromjson)[] | select(.name | startswith("_")|not)| select(.name | startswith(    "@")|not)|"|   " + .name + "   |   " + (.esTypes|tostring) + "   |   " + .type + "   |        |"'| tr -d '["]' >> $outfile
+echo "|   Fieldname                   |   ES type    |   Kibana type  |   Comment                                     | " > $outfile
+echo "| ----------------------------- | ------------ | -------------- | --------------------------------------------- | " >> $outfile
+curl -s -X GET "localhost:5601/api/saved_objects/index-pattern/${type}" -H 'kbn-xsrf: true' | jq -r '(.attributes.fields | fromjson)[] | select(.name | startswith("_")|not)| select(.name | startswith("@")|not)|"| " + .name + "   | " + (.esTypes|tostring) + "   | " + .type + "   |        |"'| tr -d '["]' >> $outfile
 
 # index bluecheck
 type=bluecheck
 outfile=redelk_fieldnames_${type}.md
-echo "|   Fieldname  |  ES field type   | Kibana field type    |  Comment  |" > $outfile
-echo "| --- | --- | --- | --- |" >> $outfile
-curl -s -X GET "localhost:5601/api/saved_objects/index-pattern/${type}" -H 'kbn-xsrf: true' | jq -r '(.attributes.fields | fromjson)[] | select(.name | startswith("_")|not)| select(.name | startswith(    "@")|not)|"|   " + .name + "   |   " + (.esTypes|tostring) + "   |   " + .type + "   |        |"'| tr -d '["]' >> $outfile
+echo "|   Fieldname                   |   ES type    |   Kibana type  |   Comment                                     | " > $outfile
+echo "| ----------------------------- | ------------ | -------------- | --------------------------------------------- | " >> $outfile
+curl -s -X GET "localhost:5601/api/saved_objects/index-pattern/${type}" -H 'kbn-xsrf: true' | jq -r '(.attributes.fields | fromjson)[] | select(.name | startswith("_")|not)| select(.name | startswith("@")|not)|"| " + .name + "   | " + (.esTypes|tostring) + "   | " + .type + "   |        |"'| tr -d '["]' >> $outfile
 
 # index credentials
 type=credentials
 outfile=redelk_fieldnames_${type}.md
-echo "|   Fieldname  |  ES field type   | Kibana field type    |  Comment  |" > $outfile
-echo "| --- | --- | --- | --- |" >> $outfile
-curl -s -X GET "localhost:5601/api/saved_objects/index-pattern/${type}" -H 'kbn-xsrf: true' | jq -r '(.attributes.fields | fromjson)[] | select(.name | startswith("_")|not)| select(.name | startswith(    "@")|not)|"|   " + .name + "   |   " + (.esTypes|tostring) + "   |   " + .type + "   |        |"'| tr -d '["]' >> $outfile
+echo "|   Fieldname                   |   ES type    |   Kibana type  |   Comment                                     | " > $outfile
+echo "| ----------------------------- | ------------ | -------------- | --------------------------------------------- | " >> $outfile
+curl -s -X GET "localhost:5601/api/saved_objects/index-pattern/${type}" -H 'kbn-xsrf: true' | jq -r '(.attributes.fields | fromjson)[] | select(.name | startswith("_")|not)| select(.name | startswith("@")|not)|"| " + .name + "   | " + (.esTypes|tostring) + "   | " + .type + "   |        |"'| tr -d '["]' >> $outfile
 
 # index bluecheck
 type=bluecheck
 outfile=redelk_fieldnames_${type}.md
-echo "|   Fieldname  |  ES field type   | Kibana field type    |  Comment  |" > $outfile
-echo "| --- | --- | --- | --- |" >> $outfile
-curl -s -X GET "localhost:5601/api/saved_objects/index-pattern/${type}" -H 'kbn-xsrf: true' | jq -r '(.attributes.fields | fromjson)[] | select(.name | startswith("_")|not)| select(.name | startswith(    "@")|not)|"|   " + .name + "   |   " + (.esTypes|tostring) + "   |   " + .type + "   |        |"'| tr -d '["]' >> $outfile
+echo "|   Fieldname                   |   ES type    |   Kibana type  |   Comment                                     | " > $outfile
+echo "| ----------------------------- | ------------ | -------------- | --------------------------------------------- | " >> $outfile
+curl -s -X GET "localhost:5601/api/saved_objects/index-pattern/${type}" -H 'kbn-xsrf: true' | jq -r '(.attributes.fields | fromjson)[] | select(.name | startswith("_")|not)| select(.name | startswith("@")|not)|"| " + .name + "   | " + (.esTypes|tostring) + "   | " + .type + "   |        |"'| tr -d '["]' >> $outfile
 
 # index email
 type=email
 outfile=redelk_fieldnames_${type}.md
-echo "|   Fieldname  |  ES field type   | Kibana field type    |  Comment  |" > $outfile
-echo "| --- | --- | --- | --- |" >> $outfile
-curl -s -X GET "localhost:5601/api/saved_objects/index-pattern/${type}" -H 'kbn-xsrf: true' | jq -r '(.attributes.fields | fromjson)[] | select(.name | startswith("_")|not)| select(.name | startswith(    "@")|not)|"|   " + .name + "   |   " + (.esTypes|tostring) + "   |   " + .type + "   |        |"'| tr -d '["]' >> $outfile
+echo "|   Fieldname                   |   ES type    |   Kibana type  |   Comment                                     | " > $outfile
+echo "| ----------------------------- | ------------ | -------------- | --------------------------------------------- | " >> $outfile
+curl -s -X GET "localhost:5601/api/saved_objects/index-pattern/${type}" -H 'kbn-xsrf: true' | jq -r '(.attributes.fields | fromjson)[] | select(.name | startswith("_")|not)| select(.name | startswith("@")|not)|"| " + .name + "   | " + (.esTypes|tostring) + "   | " + .type + "   |        |"'| tr -d '["]' >> $outfile
 
 # index implantsdb
 type=implantsdb
 outfile=redelk_fieldnames_${type}.md
-echo "|   Fieldname  |  ES field type   | Kibana field type    |  Comment  |" > $outfile
-echo "| --- | --- | --- | --- |" >> $outfile
-curl -s -X GET "localhost:5601/api/saved_objects/index-pattern/${type}" -H 'kbn-xsrf: true' | jq -r '(.attributes.fields | fromjson)[] | select(.name | startswith("_")|not)| select(.name | startswith(    "@")|not)|"|   " + .name + "   |   " + (.esTypes|tostring) + "   |   " + .type + "   |        |"'| tr -d '["]' >> $outfile
+echo "|   Fieldname                   |   ES type    |   Kibana type  |   Comment                                     | " > $outfile
+echo "| ----------------------------- | ------------ | -------------- | --------------------------------------------- | " >> $outfile
+curl -s -X GET "localhost:5601/api/saved_objects/index-pattern/${type}" -H 'kbn-xsrf: true' | jq -r '(.attributes.fields | fromjson)[] | select(.name | startswith("_")|not)| select(.name | startswith("@")|not)|"| " + .name + "   | " + (.esTypes|tostring) + "   | " + .type + "   |        |"'| tr -d '["]' >> $outfile
