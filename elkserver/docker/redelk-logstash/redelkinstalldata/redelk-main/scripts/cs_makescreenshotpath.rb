3708246a1a9a540ca9d9a09abbff887da93fa80d
Marc Smeets <marc@outflank.nl>
2020-11-13 14:37:31 +0100

    CS4.2 screenshot parsing update

--- elkserver/docker/redelk-logstash/redelkinstalldata/redelk-main/scripts/cs_makescreenshotpath.rb
+++ elkserver/docker/redelk-logstash/redelkinstalldata/redelk-main/scripts/cs_makescreenshotpath.rb
@@ -1,21 +1,19 @@
 #
 # Part of RedELK
 # Script to have logstash insert extra fields pointing to the Cobalt Strike screenshots
+# Cobalt Strike 4.2 and higher
 #
 # Author: Outflank B.V. / Marc Smeets
 #
 
 def filter(event)
-  require 'time'
   host = event.get("[agent][name]")
   logpath = event.get("[log][file][path]")
-  implant_id = event.get("[implant][id]")
-  timefromcs = event.get("[c2][timestamp]") + " UTC"
-  timestring =  Time.parse(timefromcs).strftime("%I%M%S")
+  filename = event.get("[screenshot][file_name]")
   temppath = logpath.split('/cobaltstrike')
   temppath2 = temppath[1].split(/\/([^\/]*)$/)
-  screenshoturl = "/c2logs/" + "#{host}" + "#{temppath2[0]}" + "/screenshots/screen_" + "#{timestring}" + "_" + "#{implant_id}" + ".jpg"
-  thumburl = "/c2logs/" + "#{host}" + "#{temppath2[0]}" + "/screenshots/screen_" + "#{timestring}" + "_" + "#{implant_id}" + ".jpg.thumb.jpg"
+  screenshoturl = "/c2logs/" + "#{host}" + "#{temppath2[0]}" + "/screenshots/"+ "#{filename}"
+  thumburl = "/c2logs/" + "#{host}" + "#{temppath2[0]}" + "/screenshots/"+ "#{filename}" + ".thumb.jpg"
   event.tag("_rubyparseok")
   event.set("[screenshot][full]", screenshoturl)
   event.set("[screenshot][thumb]", thumburl)
