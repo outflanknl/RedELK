054843021f0677fbb4b77aed113597f86c1b55a6
Kadir Yamamoto <me@yamakadi.com>
2021-01-14 18:30:53 +0900

    Change the wording of the message regarding 4th parameter

--- elkserver/docker/redelk-base/redelkinstalldata/scripts/getremotelogs.sh
+++ elkserver/docker/redelk-base/redelkinstalldata/scripts/getremotelogs.sh
@@ -13,7 +13,7 @@ if [ $# -lt 3 ] ; then
     echo "[X] Error - need IP or full DNS name of system to get remote logs from as 1st parameter."
     echo "[X] Error - need the remote system's filebeats hostname as 2nd parameter."
     echo "[X] Error - need the username to connect with as 3rd parameter."
-    echo "[X] Error - need the ssh port of system to get remote logs from as 4th parameter. Defaults to '22'."
+    echo "[X] Optional - need the remote system's ssh port to get logs as 4th parameter. Defaults to '22'"
     echo "Incorrect amount of parameters" >> $LOGFILE 2>&1
     exit 1
 fi
