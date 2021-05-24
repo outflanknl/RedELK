2501cc4bef862086175195304e9c61c46cc08f74
fastlorenzo <git@bernardi.be>
2021-05-24 22:52:31 +0200

    Added pylint
    Signed-off-by: fastlorenzo <git@bernardi.be>

--- elkserver/docker/redelk-base/redelkinstalldata/scripts/daemon.py
+++ elkserver/docker/redelk-base/redelkinstalldata/scripts/daemon.py
@@ -1,152 +1,155 @@
 #!/usr/bin/python3
-#
+"""
 # Part of RedELK
 # Script to check if there are alarms to be sent
 #
 # Authors:
 # - Outflank B.V. / Mark Bergman (@xychix)
 # - Lorenzo Bernardi (@fastlorenzo)
-#
-import os
+"""
+
+import copy
 import importlib
 import logging
-import copy
+import os
 
-from modules.helpers import shouldModuleRun, setTags, moduleDidRun, addAlarmData, groupHits
-from config import alarms, notifications, loglevel
+from config import alarms, loglevel, notifications
+from modules.helpers import (addAlarmData, groupHits, moduleDidRun, setTags,
+                             shouldModuleRun)
 
+MODULES_PATH = './modules/'
 
-# Attempt to load the different modules in their respective dictionaries, and return them
 def load_modules():
-    aD = {}  # aD alarm Dict
-    cD = {}  # cD connector Dict
-    eD = {}  # eD enrich Dict
+    """ Attempt to load the different modules in their respective dictionaries, and return them """
+    alarm_dict = {}  # aD alarm Dict
+    connector_dict = {}  # cD connector Dict
+    enrich_dict = {}  # eD enrich Dict
+    module_folders = os.listdir(MODULES_PATH)
 
     for module in module_folders:
         # only take folders and not '__pycache__'
-        if os.path.isdir(os.path.join(path, module)) and module != '__pycache__':
+        if os.path.isdir(os.path.join(MODULES_PATH, module)) and module != '__pycache__':
             try:
-                m = importlib.import_module(
+                module = importlib.import_module(
                     'modules.%s.%s' % (module, 'module'))
-                if (hasattr(m, 'info') and hasattr(m, 'Module')):
-                    module_type = m.info.get('type', None)
+                if (hasattr(module, 'info') and hasattr(module, 'Module')):
+                    module_type = module.info.get('type', None)
                     if module_type == 'redelk_alarm':
-                        aD[module] = {}
-                        aD[module]['info'] = m.info
-                        aD[module]['m'] = m
-                        aD[module]['status'] = 'pending'
+                        alarm_dict[module] = {}
+                        alarm_dict[module]['info'] = module.info
+                        alarm_dict[module]['m'] = module
+                        alarm_dict[module]['status'] = 'pending'
                     elif module_type == 'redelk_connector':
-                        cD[module] = {}
-                        cD[module]['info'] = m.info
-                        cD[module]['m'] = m
-                        cD[module]['status'] = 'pending'
+                        connector_dict[module] = {}
+                        connector_dict[module]['info'] = module.info
+                        connector_dict[module]['m'] = module
+                        connector_dict[module]['status'] = 'pending'
                     elif module_type == 'redelk_enrich':
-                        eD[module] = {}
-                        eD[module]['info'] = m.info
-                        eD[module]['m'] = m
-                        eD[module]['status'] = 'pending'
-            except Exception as e:
-                logger.error('Error in module %s: %s' % (module, e))
-                logger.exception(e)
-                pass
-    return(aD, cD, eD)
+                        enrich_dict[module] = {}
+                        enrich_dict[module]['info'] = module.info
+                        enrich_dict[module]['m'] = module
+                        enrich_dict[module]['status'] = 'pending'
+            # pylint: disable=broad-except
+            except Exception as error:
+                logger.error('Error in module %s: %s', module, error)
+                logger.exception(error)
+    return(alarm_dict, connector_dict, enrich_dict)
 
-
-# Run the different enrichment scripts that are enabled
-def run_enrichments(eD):
+def run_enrichments(enrich_dict):
+    """ Run the different enrichment scripts that are enabled """
     logger.info('Running enrichment modules')
     # First loop through the enrichment modules
-    for e in eD:
-        if shouldModuleRun(e, 'redelk_enrich'):
+    for enrich_module in enrich_dict:
+        if shouldModuleRun(enrich_module, 'redelk_enrich'):
             try:
-                logger.debug('[e] initiating class Module() in %s' % e)
-                moduleClass = eD[e]['m'].Module()
-                logger.debug('[e] Running Run() from the Module class in %s' % e)
-                eD[e]['result'] = copy.deepcopy(moduleClass.run())
+                logger.debug('[e] initiating class Module() in %s', enrich_module)
+                module_class = enrich_dict[enrich_module]['m'].Module()
+                logger.debug('[e] Running Run() from the Module class in %s', enrich_module)
+                enrich_dict[enrich_module]['result'] = copy.deepcopy(module_class.run())
 
                 # Now loop through the hits and tag them
-                for rHit in eD[e]['result']['hits']['hits']:
-                    setTags(eD[e]['info']['submodule'], [rHit])
+                for hit in enrich_dict[enrich_module]['result']['hits']['hits']:
+                    setTags(enrich_dict[enrich_module]['info']['submodule'], [hit])
 
-                hits = len(eD[e]['result']['hits']['hits'])
-                moduleDidRun(e, 'enrich', 'success', 'Enriched %s documents' % hits, hits)
-                eD[e]['status'] = 'success'
+                hits = len(enrich_dict[enrich_module]['result']['hits']['hits'])
+                moduleDidRun(enrich_module, 'enrich', 'success', 'Enriched %s documents' % hits, hits)
+                enrich_dict[enrich_module]['status'] = 'success'
+            # pylint: disable=broad-except
             except Exception as err:
-                msg = 'Error running enrichment %s: %s' % (e, err)
+                msg = 'Error running enrichment %s: %s' % (enrich_module, err)
                 logger.error(msg)
                 logger.exception(err)
-                moduleDidRun(e, 'enrich', 'error', msg)
-                eD[e]['status'] = 'error'
-    return(eD)
+                moduleDidRun(enrich_module, 'enrich', 'error', msg)
+                enrich_dict[enrich_module]['status'] = 'error'
+    return enrich_dict
 
-
-# Run the different alarm scripts that are enabled and return the results
-def run_alarms(aD):
+def run_alarms(alarm_dict):
+    """ Run the different alarm scripts that are enabled and return the results """
     logger.info('Running alarm modules')
     # this means we've loaded the modules and will now loop over those one by one
-    for a in aD:
-        if shouldModuleRun(a, 'redelk_alarm'):
+    for alarm_module in alarm_dict:
+        if shouldModuleRun(alarm_module, 'redelk_alarm'):
             try:
-                logger.debug('[a] initiating class Module() in %s' % a)
-                moduleClass = aD[a]['m'].Module()
-                logger.debug('[a] Running Run() from the Module class in %s' % a)
-                aD[a]['result'] = copy.deepcopy(moduleClass.run())
-                hits = len(aD[a]['result']['hits']['hits'])
-                moduleDidRun(a, 'alarm', 'success', 'Found %s documents to alarm' % hits, hits)
-                aD[a]['status'] = 'success'
-            except Exception as e:
-                msg = 'Error running alarm %s: %s' % (a, e)
+                logger.debug('[a] initiating class Module() in %s', alarm_module)
+                module_class = alarm_dict[alarm_module]['m'].Module()
+                logger.debug('[a] Running Run() from the Module class in %s', alarm_module)
+                alarm_dict[alarm_module]['result'] = copy.deepcopy(module_class.run())
+                hits = len(alarm_dict[alarm_module]['result']['hits']['hits'])
+                moduleDidRun(alarm_module, 'alarm', 'success', 'Found %s documents to alarm' % hits, hits)
+                alarm_dict[alarm_module]['status'] = 'success'
+            # pylint: disable=broad-except
+            except Exception as error:
+                msg = 'Error running alarm %s: %s' % (alarm_module, error)
                 logger.error(msg)
-                logger.exception(e)
-                moduleDidRun(a, 'alarm', 'error', msg)
-                aD[a]['status'] = 'error'
-    return(aD)
+                logger.exception(error)
+                moduleDidRun(alarm_module, 'alarm', 'error', msg)
+                alarm_dict[alarm_module]['status'] = 'error'
+    return alarm_dict
 
-
-# Process the alarm results and send notifications via connector modules
-def process_alarms(cD, aD):
+def process_alarms(connector_dict, alarm_dict):
+    """ Process the alarm results and send notifications via connector modules """
     logger.info('Processing alarms')
     # now we can loop over the modules once again and log the lines
-    for a in aD:
-        if a in alarms and alarms[a]['enabled']:
+    for alarm in alarm_dict:
+        if alarm in alarms and alarms[alarm]['enabled']:
 
             # If the alarm did fail to run, skip processing the notification and tagging as we are not sure of the results
-            if aD[a]['status'] != 'success':
-                logger.warn('Alarm %s did not run (correctly), skipping processing' % a)
+            if alarm_dict[alarm]['status'] != 'success':
+                logger.warning('Alarm %s did not run (correctly), skipping processing', alarm)
                 continue
 
-            logger.debug('Alarm %s enabled, processing hits' % a)
-            r = aD[a]['result']
-            alarm_name = aD[a]['info']['submodule']
+            logger.debug('Alarm %s enabled, processing hits', alarm)
+            result = alarm_dict[alarm]['result']
+            alarm_name = alarm_dict[alarm]['info']['submodule']
             # logger.debug('Alarm results: %s' % aD[a]['result'])
-            for rHit in r['hits']['hits']:
+            for result_hits in result['hits']['hits']:
                 # First check if there is a mutation data to add
-                logger.debug(rHit)
-                if rHit['_id'] in r['mutations']:
-                    m = r['mutations'][rHit['_id']]
+                logger.debug(result_hits)
+                if result_hits['_id'] in result['mutations']:
+                    mutations = result['mutations'][result_hits['_id']]
                 else:
-                    m = {}
+                    mutations = {}
                 # And now, let's add mutations data to the doc and update back the hits
-                rHit = addAlarmData(rHit, m, alarm_name)
+                result_hits = addAlarmData(result_hits, mutations, alarm_name)
 
             # Let's tag the docs with the alarm name
-            setTags(alarm_name, r['hits']['hits'])
-            logger.debug('calling settags %s (%d hits)' % (alarm_name, r['hits']['total']))
+            setTags(alarm_name, result['hits']['hits'])
+            logger.debug('calling settags %s (%d hits)', alarm_name, result['hits']['total'])
 
             # Needed as groupHits will change r['hits']['hits'] and different alarms might do different grouping
-            r = copy.deepcopy(aD[a]['result'])
-            if r['hits']['total'] > 0:
+            result = copy.deepcopy(alarm_dict[alarm]['result'])
+            if result['hits']['total'] > 0:
                 # Group the hits before sending it to the alarm, based on the 'groubpby' array returned by the alarm
-                gb = list(r['groupby'])
-                r['hits']['hits'] = groupHits(r['hits']['hits'], gb)
+                group_by = list(result['groupby'])
+                result['hits']['hits'] = groupHits(result['hits']['hits'], group_by)
 
-                for c in cD:
+                for connector in connector_dict:
                     # connector will process ['hits']['hits'] which contains a list of 'jsons' looking like an ES line
                     # connector will report the fields in ['hits']['fields'] for each of the lines in the list
-                    if c in notifications and notifications[c]['enabled']:
-                        connector = cD[c]['m'].Module()
-                        logger.info('connector %s enabled, sending alarm (%d hits)' % (c, r['hits']['total']))
-                        connector.send_alarm(r)
+                    if connector in notifications and notifications[connector]['enabled']:
+                        connector_mod = connector_dict[connector]['m'].Module()
+                        logger.info('connector %s enabled, sending alarm (%d hits)', connector, result['hits']['total'])
+                        connector_mod.send_alarm(result)
 
 
 # Main entry point of the file
@@ -154,12 +157,6 @@ if __name__ == '__main__':
     logging.basicConfig(
         format='%(asctime)s - %(levelname)s - %(name)s - %(filename)s - %(funcName)s -- %(message)s', level=loglevel)
     logger = logging.getLogger('alarm')
-    path = './modules/'
-    module_folders = os.listdir(path)
-    logger.debug(module_folders)
-
-    connectors_path = './modules/'
-    connectors_folders = os.listdir(connectors_path)
 
     # 1. Load all modules
     (aD, cD, eD) = load_modules()
