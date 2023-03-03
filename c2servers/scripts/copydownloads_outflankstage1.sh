6d07e635c20e6632449a64cc77956efe9a2a7dc4
Marc Smeets <marc@outflank.nl>
2023-03-03 16:29:21 +0100

    updated implant log path in file copy script

--- c2servers/scripts/copydownloads_outflankstage1.sh
+++ c2servers/scripts/copydownloads_outflankstage1.sh
@@ -14,7 +14,7 @@ mkdir -p /home/scponly/stage1/downloads >> $LOGFILE 2>&1
 echo "`date` ######## Start downloads copy" >> $LOGFILE 2>&1
 
 for fileid in $(ls /root/stage1c2server/shared/downloads/ | grep -v '\.'); do
-  orifilename=`grep -rn $fileid /root/stage1c2server/shared/logs/stage1/*|awk 'BEGIN {FS="taskResponse: Downloaded"}; {print $2}'|awk -F"] " '{print $2}'|tr -d ";"|awk '{$1=$1};1'`
+  orifilename=`grep -rn $fileid /root/stage1c2server/shared/logs/api/implant_logs/legacy_text/*|awk 'BEGIN {FS="taskResponse: Downloaded"}; {print $2}'|awk -F"] " '{print $2}'|tr -d ";"|awk '{$1=$1};1'`
   if [ ! -f "/home/scponly/stage1/downloads/${fileid}_${orifilename}" ]; then
     cp /root/stage1c2server/shared/downloads/${fileid} "/home/scponly/stage1/downloads/${fileid}_${orifilename}"
     chown scponly:scponly "/home/scponly/stage1/downloads/${fileid}_${orifilename}"
