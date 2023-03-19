404d1640f86eb3b1e64e99617b02492581128d47
fastlorenzo <git@bernardi.be>
2023-03-19 21:07:04 +0100

    Bumped Elastic Stack to 7.17.9
    Signed-off-by: fastlorenzo <git@bernardi.be>

--- redirs/install-redir.sh
+++ redirs/install-redir.sh
@@ -8,29 +8,29 @@
 
 LOGFILE="redelk-install.log"
 INSTALLER="RedELK redirector installer"
-ELKVERSION="7.16.3"
+ELKVERSION="7.17.9"
 
 #set default locale
 export LC_ALL="en_US.UTF-8"
-printf 'LANG=en_US.UTF-8\nLC_ALL=en_US.UTF-8\n' > /etc/default/locale >> $LOGFILE 2>&1
-locale-gen >> $LOGFILE 2>&1
+printf 'LANG=en_US.UTF-8\nLC_ALL=en_US.UTF-8\n' >/etc/default/locale >>$LOGFILE 2>&1
+locale-gen >>$LOGFILE 2>&1
 
 echoerror() {
-    printf "`date +'%b %e %R'` $INSTALLER - ${RC} * ERROR ${EC}: $@\n" >> $LOGFILE 2>&1
+    printf "$(date +'%b %e %R') $INSTALLER - ${RC} * ERROR ${EC}: $@\n" >>$LOGFILE 2>&1
 }
 
 preinstallcheck() {
-   echo "Starting pre installation checks"
+    echo "Starting pre installation checks"
 
     # Checking if OS is Debian / APT based
-    if [ ! -f  /etc/debian_version ]; then
+    if [ ! -f /etc/debian_version ]; then
         echo "[X] This system does not seem to be Debian/APT-based. RedELK installer only supports Debian/APT based systems."
         echoerror "System is not Debian/APT based. Not supported. Quitting."
         exit 1
     fi
 
-    if [ -n "$(dpkg -s filebeat 2>/dev/null| grep Status)" ]; then
-        INSTALLEDVERSION=`dpkg -s filebeat |grep Version|awk '{print $2}'` >> $LOGFILE 2>&1
+    if [ -n "$(dpkg -s filebeat 2>/dev/null | grep Status)" ]; then
+        INSTALLEDVERSION=$(dpkg -s filebeat | grep Version | awk '{print $2}') >>$LOGFILE 2>&1
         if [ "$INSTALLEDVERSION" != "$ELKVERSION" ]; then
             echo "[X] Filebeat: installed version $INSTALLEDVERSION, required version $ELKVERSION. Please fix manually."
             echoerror "Filebeat version mismatch. Please fix manually."
@@ -46,7 +46,7 @@ preinstallcheck() {
     fi
 }
 
-printf "`date +'%b %e %R'` $INSTALLER - Starting installer\n" > $LOGFILE 2>&1
+printf "$(date +'%b %e %R') $INSTALLER - Starting installer\n" >$LOGFILE 2>&1
 echo ""
 echo ""
 echo ""
@@ -67,7 +67,7 @@ echo ""
 #  exit 1
 #fi
 
-if ! [ $# -eq 3 ] ; then
+if ! [ $# -eq 3 ]; then
     echo "[X] ERROR Incorrect amount of parameters"
     echo "[X] require 1st parameter: identifier of this machine to set in filebeat config."
     echo "[X] require 2nd parameter: attackscenario name."
@@ -78,23 +78,23 @@ fi
 
 preinstallcheck
 
-echo "[*] Adding GPG key of Elastic"  | tee -a $LOGFILE
-wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add - >> $LOGFILE 2>&1
+echo "[*] Adding GPG key of Elastic" | tee -a $LOGFILE
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add - >>$LOGFILE 2>&1
 ERROR=$?
 if [ $ERROR -ne 0 ]; then
     echoerror "Could not add GPG key (Error Code: $ERROR)."
 fi
 
 echo "[*] Installing apt-transport-https" | tee -a $LOGFILE
-apt-get install -y apt-transport-https >> $LOGFILE 2>&1
+apt-get install -y apt-transport-https >>$LOGFILE 2>&1
 ERROR=$?
 if [ $ERROR -ne 0 ]; then
     echoerror "Could not install apt-transport-https (Error Code: $ERROR)."
 fi
 
 echo "[*] Adding Elastic APT repository" | tee -a $LOGFILE
-if [ ! -f  /etc/apt/sources.list.d/elastic-7.x.list ]; then
-    echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-7.x.list >> $LOGFILE 2>&1
+if [ ! -f /etc/apt/sources.list.d/elastic-7.x.list ]; then
+    echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-7.x.list >>$LOGFILE 2>&1
 fi
 ERROR=$?
 if [ $ERROR -ne 0 ]; then
@@ -102,71 +102,71 @@ if [ $ERROR -ne 0 ]; then
 fi
 
 echo "[*] Updating APT" | tee -a $LOGFILE
-apt-get update  >> $LOGFILE 2>&1
+apt-get update >>$LOGFILE 2>&1
 ERROR=$?
 if [ $ERROR -ne 0 ]; then
     echoerror "Could not update APT (Error Code: $ERROR)."
 fi
 
 echo "[*] Installing filebeat" | tee -a $LOGFILE
-apt-get install -y filebeat=$ELKVERSION >> $LOGFILE 2>&1
+apt-get install -y filebeat=$ELKVERSION >>$LOGFILE 2>&1
 ERROR=$?
 if [ $ERROR -ne 0 ]; then
     echoerror "[X] Could not install filebeat (Error Code: $ERROR)."
 fi
 
 echo "[*] Setting filebeat to auto start after reboot" | tee -a $LOGFILE
-systemctl enable filebeat >> $LOGFILE 2>&1
+systemctl enable filebeat >>$LOGFILE 2>&1
 ERROR=$?
 if [ $ERROR -ne 0 ]; then
     echoerror "[X] Could not change auto boot settings (Error Code: $ERROR)."
 fi
 
 echo "[*] Making backup of original filebeat config" | tee -a $LOGFILE
-mv /etc/filebeat/filebeat.yml /etc/filebeat/filebeat.yml.ori >> $LOGFILE 2>&1
+mv /etc/filebeat/filebeat.yml /etc/filebeat/filebeat.yml.ori >>$LOGFILE 2>&1
 ERROR=$?
 if [ $ERROR -ne 0 ]; then
     echoerror "[X] Could not make backup (Error Code: $ERROR)."
 fi
 
 echo "[*] Copying new config file" | tee -a $LOGFILE
-cp ./filebeat/filebeat.yml /etc/filebeat/ >> $LOGFILE 2>&1
+cp ./filebeat/filebeat.yml /etc/filebeat/ >>$LOGFILE 2>&1
 ERROR=$?
 if [ $ERROR -ne 0 ]; then
     echoerror "[X] Could not copy filebeat config (Error Code: $ERROR)."
 fi
 
 echo "[*] Copying ca file" | tee -a $LOGFILE
-cp ./filebeat/redelkCA.crt /etc/filebeat/ >> $LOGFILE 2>&1
+cp ./filebeat/redelkCA.crt /etc/filebeat/ >>$LOGFILE 2>&1
 ERROR=$?
 if [ $ERROR -ne 0 ]; then
     echoerror "[X] Could not copy ca file (Error Code: $ERROR)."
 fi
 
 echo "[*] Altering hostname field in filebeat config" | tee -a $LOGFILE
-sed -i s/'@@HOSTNAME@@'/$1/g /etc/filebeat/filebeat.yml  >> $LOGFILE 2>&1
+sed -i s/'@@HOSTNAME@@'/$1/g /etc/filebeat/filebeat.yml >>$LOGFILE 2>&1
 ERROR=$?
 if [ $ERROR -ne 0 ]; then
     echoerror "[X] Could not change hostname field in filebeat config (Error Code: $ERROR)."
 fi
 
 echo "[*] Altering attackscenario field in filebeat config" | tee -a $LOGFILE
-sed -i s/'@@ATTACKSCENARIO@@'/$2/g /etc/filebeat/filebeat.yml >> $LOGFILE 2>&1
+sed -i s/'@@ATTACKSCENARIO@@'/$2/g /etc/filebeat/filebeat.yml >>$LOGFILE 2>&1
 ERROR=$?
 if [ $ERROR -ne 0 ]; then
     echoerror "[X] Could not change attackscenario field in filebeat config (Error Code: $ERROR)."
 fi
 
 echo "[*] Altering log destination field in filebeat config" | tee -a $LOGFILE
-sed -i s/'@@HOSTANDPORT@@'/$3/g /etc/filebeat/filebeat.yml >> $LOGFILE 2>&1
+sed -i s/'@@HOSTANDPORT@@'/$3/g /etc/filebeat/filebeat.yml >>$LOGFILE 2>&1
 ERROR=$?
 if [ $ERROR -ne 0 ]; then
     echoerror "[X] Could not change log destination field in filebeat config (Error Code: $ERROR)."
 fi
 
 echo "[*] Altering logrotate settings for HAProxy - rotate weekly instead of daily" | tee -a $LOGFILE
-if [ -f  /etc/logrotate.d/haproxy ]; then
-    sed -i s/'daily'/'weekly'/g /etc/logrotate.d/haproxy >> $LOGFILE 2>&1
+if [ -f /etc/logrotate.d/haproxy ]; then
+    sed -i s/'daily'/'weekly'/g /etc/logrotate.d/haproxy >>$LOGFILE 2>&1
     ERROR=$?
     if [ $ERROR -ne 0 ]; then
         echoerror "[X] Could not change logrotate settings for HAProxy (Error Code: $ERROR). "
@@ -174,7 +174,7 @@ if [ -f  /etc/logrotate.d/haproxy ]; then
 fi
 
 echo "[*] Starting filebeat" | tee -a $LOGFILE
-service filebeat start >> $LOGFILE 2>&1
+service filebeat start >>$LOGFILE 2>&1
 ERROR=$?
 if [ $ERROR -ne 0 ]; then
     echoerror "[X] Could not start filebeat (Error Code: $ERROR)."
